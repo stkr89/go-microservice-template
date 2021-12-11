63e707115290f65f596c1f67aade00518e7b8c01
stkr89 <OOswvwnnn@007>
2021-12-11 06:53:33 -0600

    add templates for integration test cases

--- test/grpc_integration_test.go
+++ test/grpc_integration_test.go
@@ -1,1 +1,44 @@
 package test
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/shopr-org/grpc-service-template/cmd/server"
+	"github.com/shopr-org/grpc-service-template/common"
+	"github.com/shopr-org/grpc-service-template/endpoints"
+	"github.com/shopr-org/grpc-service-template/pb"
+	"github.com/shopr-org/grpc-service-template/service"
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc"
+	"os"
+	"testing"
+)
+
+type GRPCIntegrationTestSuite struct {
+	suite.Suite
+	conn   *grpc.ClientConn
+	client pb.MathServiceClient
+}
+
+func (suite *GRPCIntegrationTestSuite) SetupSuite() {
+	err := godotenv.Load("../.env")
+	suite.NoError(err)
+
+	e := endpoints.MakeEndpoints(service.NewMathServiceImpl())
+	server.StartServer(common.NewLogger(), e, true, false)
+
+	conn, err := grpc.Dial(fmt.Sprintf(":%s", os.Getenv("GRPC_PORT")), grpc.WithInsecure())
+	suite.NoError(err)
+	suite.conn = conn
+	suite.client = pb.NewMathServiceClient(conn)
+}
+
+func (suite *GRPCIntegrationTestSuite) TearDownTestSuite() {
+	_ = suite.conn.Close()
+}
+
+// new test cases
+
+func TestGRPCIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(GRPCIntegrationTestSuite))
+}
--- test/http_integration_test.go
+++ test/http_integration_test.go
@@ -1,1 +1,34 @@
 package test
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/shopr-org/grpc-service-template/cmd/server"
+	"github.com/shopr-org/grpc-service-template/common"
+	"github.com/shopr-org/grpc-service-template/endpoints"
+	"github.com/shopr-org/grpc-service-template/service"
+	transport "github.com/shopr-org/grpc-service-template/transports"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"testing"
+)
+
+type HTTPIntegrationTestSuite struct {
+	suite.Suite
+	handler http.Handler
+}
+
+func (suite *HTTPIntegrationTestSuite) SetupSuite() {
+	err := godotenv.Load("../.env")
+	suite.NoError(err)
+
+	e := endpoints.MakeEndpoints(service.NewMathServiceImpl())
+	server.StartServer(common.NewLogger(), e, false, true)
+
+	suite.handler = transport.NewHTTPHandler(e)
+}
+
+// new test cases
+
+func TestHTTPIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(HTTPIntegrationTestSuite))
+}
