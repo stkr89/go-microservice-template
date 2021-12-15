ebd88d007c5713140fa6a99e767bce4eda39ffb2
stkr89 <OOswvwnnn@007>
2021-12-14 19:15:18 -0600

    disable gorm logging

--- config/db.go
+++ config/db.go
@@ -3,12 +3,15 @@ package config
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"os"
 )
 
 func NewDB() gorm.DB {
 	dbURL := os.ExpandEnv("postgresql://$DB_USERNAME:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?&options=--cluster%3D$DB_CLUSTER")
-	db, err := gorm.Open(postgres.Open(dbURL))
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		panic(err)
 	}
